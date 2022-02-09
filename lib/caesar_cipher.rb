1842105354ea9b82f97b37017272039018c0c8ee
Thaddeus Brown <thaddeusmbrown@gmail.com>
2022-02-08 20:22:26 -0600

    linted with rubocop and confirmed tests still passed

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,32 +1,30 @@
+# frozen_string_literal: true
+
 def caesar_cipher(message, shift_factor)
-    
-    # convert shift_factor to number between 1 and 26 to simplify encryption step
-    shift_factor = shift_factor % 26
+  # convert shift_factor to number between 1 and 26 to simplify encryption step
+  shift_factor = shift_factor % 26
 
-    # convert message input to ASCII encoding
-    message_ascii = message.bytes
+  # convert message input to ASCII encoding
+  message_ascii = message.bytes
 
-    #go through each character, see if it is a letter, and shift it based on shift_factor
-    message_ascii_shifted = []
+  # go through each character, see if it is a letter, and shift it based on shift_factor
+  message_ascii_shifted = []
 
-    message_ascii.each do |character|
-
-        if character.between?(65, 90) && character + shift_factor <= 90 # shift uppercase characters that don't overflow
-            message_ascii_shifted.push(character + shift_factor)
-        elsif character.between?(65, 90) # shift uppercase characters that do overflow
-            message_ascii_shifted.push((character + shift_factor) % 90 + 64)
-        elsif character.between?(97, 122) && character + shift_factor <= 122 #shift lowercase characters that don't overflow
-            message_ascii_shifted.push(character + shift_factor)
-        elsif character.between?(97, 122) # shift lowercase characters that do overflow
-            message_ascii_shifted.push((character + shift_factor) % 122 + 96)
-        else # push any non-letter characters into the string array
-            message_ascii_shifted.push(character)
-        end
-
+  message_ascii.each do |character|
+    if character.between?(65, 90) && character + shift_factor <= 90 # shift uppercase characters that don't overflow
+      message_ascii_shifted.push(character + shift_factor)
+    elsif character.between?(65, 90) # shift uppercase characters that do overflow
+      message_ascii_shifted.push((character + shift_factor) % 90 + 64)
+    elsif character.between?(97, 122) && character + shift_factor <= 122 # shift lowercase characters that don't overflow
+      message_ascii_shifted.push(character + shift_factor)
+    elsif character.between?(97, 122) # shift lowercase characters that do overflow
+      message_ascii_shifted.push((character + shift_factor) % 122 + 96)
+    else # push any non-letter characters into the string array
+      message_ascii_shifted.push(character)
     end
+  end
 
-    #return the encrypted message
-    message_shifted = message_ascii_shifted.map { |character| character.chr }
-    message_shifted = message_shifted.join('')
-    message_shifted
+  # return the encrypted message
+  message_shifted = message_ascii_shifted.map(&:chr)
+  message_shifted.join('')
 end
--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,27 +1,29 @@
-require './lib/caesar_cipher.rb'
+# frozen_string_literal: true
 
-describe "#caesar_cipher" do
-  it "correctly shifts a string of lowercase letters" do
-    key = "what a string!"
-    hash = "xibu b tusjoh!"
+require './lib/caesar_cipher'
+
+describe '#caesar_cipher' do
+  it 'correctly shifts a string of lowercase letters' do
+    key = 'what a string!'
+    hash = 'xibu b tusjoh!'
     expect(caesar_cipher(key, 1)).to eq(hash)
   end
 
-  it "correctly shifts a string of uppercase letters" do
-    key = "WHAT A STRING!"
-    hash = "XIBU B TUSJOH!"
+  it 'correctly shifts a string of uppercase letters' do
+    key = 'WHAT A STRING!'
+    hash = 'XIBU B TUSJOH!'
     expect(caesar_cipher(key, 1)).to eq(hash)
   end
 
-  it "correctly shifts a string of mixed-case letters" do
-    key = "What a string!"
-    hash = "Xibu b tusjoh!"
+  it 'correctly shifts a string of mixed-case letters' do
+    key = 'What a string!'
+    hash = 'Xibu b tusjoh!'
     expect(caesar_cipher(key, 1)).to eq(hash)
   end
 
-  it "correctly wraps from z to a" do
-    key = "What a string!"
-    hash = "Bmfy f xywnsl!"
+  it 'correctly wraps from z to a' do
+    key = 'What a string!'
+    hash = 'Bmfy f xywnsl!'
     expect(caesar_cipher(key, 5)).to eq(hash)
   end
 end
